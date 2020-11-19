feeec2ad5fe8e25a787f71b1e66bc337184524e7
Cesar Eduardo Ortíz <pkgears@gmail.com>
2020-11-18 23:16:36 -0600

    Install annotate gem

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,23 @@
 # frozen_string_literal: true
 
-# Address model
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id               :bigint           not null, primary key
+#  address          :string
+#  addressable_type :string
+#  city             :string
+#  state            :string
+#  zip              :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  addressable_id   :bigint
+#
+# Indexes
+#
+#  index_addresses_on_addressable_type_and_addressable_id  (addressable_type,addressable_id)
+#
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
   validates :address, :city, :state, :zip, presence: true
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id               :bigint           not null, primary key
+#  address          :string
+#  addressable_type :string
+#  city             :string
+#  state            :string
+#  zip              :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  addressable_id   :bigint
+#
+# Indexes
+#
+#  index_addresses_on_addressable_type_and_addressable_id  (addressable_type,addressable_id)
+#
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
