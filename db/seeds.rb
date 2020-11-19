ede2e995c6b52b6e1489bb6907ad40937cfdd231
Cesar Eduardo Ortíz <pkgears@gmail.com>
2020-11-18 23:49:24 -0600

    Add gifts to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+%w[MUG T_SHIRT HOODIE STICKER].each{ |gift| Gift.create(name: gift) }
