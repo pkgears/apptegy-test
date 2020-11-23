96ae284614b27fbce14e4d826697f2ffb33dc3b2
Cesar Eduardo Ortíz <pkgears@gmail.com>
2020-11-22 18:36:23 -0600

    Setup User authentication

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,27 +6,24 @@
 #
 #  id                     :bigint           not null, primary key
 #  allow_password_change  :boolean          default(FALSE)
-#  confirmation_sent_at   :datetime
-#  confirmation_token     :string
-#  confirmed_at           :datetime
-#  email                  :string
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
+#  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  image                  :string
-#  name                   :string
-#  nickname               :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
 #  provider               :string           default("email"), not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
 #  tokens                 :json
 #  uid                    :string           default(""), not null
-#  unconfirmed_email      :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
 # Indexes
 #
-#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #  index_users_on_uid_and_provider      (uid,provider) UNIQUE
